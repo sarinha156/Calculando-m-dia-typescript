0377a4847b41e30257be99b7b1bb6cd79ea0641a
Sarah Cristina <sarah.brina@hotmail.com>
2019-08-14 19:59:14 -0300

    Calcular media typescript

---
+++ impl/revisao.js
@@ -0,0 +1,10 @@
+var n1 = 10;
+var n2 = 4;
+var n3 = 9;
+var media = (n1 + n2 + n3) / 3;
+if (media >= 7) {
+    console.log("Aprovado, sua média é  " + media);
+}
+else {
+    console.log("Reprovado, sua média é  " + media);
+}
---
+++ revisao.ts
@@ -0,0 +1,14 @@
+let n1 =  10
+let n2= 4
+let n3 = 9
+let media = (n1+n2+n3)/3
+
+if (media >= 7) {
+    console.log("Aprovado, sua média é  "+ media)
+    
+} 
+else {
+    console.log("Reprovado, sua média é  "+ media)
+}
+
+                              
