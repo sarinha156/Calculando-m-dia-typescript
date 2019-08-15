f799d8efcdcb17f1881d28fca8260467efd3a21e
Sarah Cristina <sarah.brina@hotmail.com>
2019-08-14 21:40:08 -0300

    Média com function

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,10 +1,17 @@
-var n1 = 10;
+var n1 = 8;
 var n2 = 4;
 var n3 = 9;
-var media = (n1 + n2 + n3) / 3;
+/*let media = (n1+n2+n3)/3
+
 if (media >= 7) {
-    console.log("Aprovado, sua média é  " + media);
+    console.log("Aprovado, sua média é  "+ media)
+    
 }
 else {
-    console.log("Reprovado, sua média é  " + media);
+    console.log("Reprovado, sua média é  "+ media)
 }
+*/
+var fmedia = function (n1, n2, n3) {
+    return (n1 + n2 + n3) / 3;
+};
+console.log("Sua media e : " + fmedia(n1, n2, n3));
