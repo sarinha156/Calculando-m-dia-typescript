9f0260d9ca1c763c610831f571266cabab5455e5
Sarah Cristina <sarah.brina@hotmail.com>
2019-08-14 21:57:07 -0300

    Modos de média com e sem function

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,9 +1,11 @@
 var n1 = 8;
 var n2 = 4;
 var n3 = 9;
+//---------- MODO FUNCTION SIMPLES ------------
 var fmedia = function (n1, n2, n3) { return (n1 + n2 + n3) / 3; };
 console.log("Sua media e : " + fmedia(n1, n2, n3));
 /*
+---------- MODO SEM FUNCTION SIMPLES ---------------
 let media = (n1+n2+n3)/3
 
 if (media >= 7) {
@@ -13,8 +15,10 @@ if (media >= 7) {
 else {
     console.log("Reprovado, sua média é  "+ media)
 }
-*/
-/*let fmedia = function(n1:number,n2:number, n3:number):number{
+
+---------- MODO COM FUNCTION COMPLICADO ------------
+
+let fmedia = function(n1:number,n2:number, n3:number):number{
     return (n1+n2+n3)/3
 }
 console.log(`Sua media e : ${fmedia (n1,n2,n3)}`)*/ 
--- revisao.ts
+++ revisao.ts
@@ -2,11 +2,13 @@ let n1: number =  8
 let n2: number = 4
 let n3: number  = 9
 
+//---------- MODO FUNCTION SIMPLES ------------
 let fmedia = (n1:number,n2:number, n3:number) => (n1+n2+n3)/3
 
 console.log(`Sua media e : ${fmedia (n1,n2,n3)}`)
 
 /*
+---------- MODO SEM FUNCTION SIMPLES ---------------
 let media = (n1+n2+n3)/3
 
 if (media >= 7) {
@@ -16,8 +18,10 @@ if (media >= 7) {
 else {
     console.log("Reprovado, sua média é  "+ media)
 }
-*/
-/*let fmedia = function(n1:number,n2:number, n3:number):number{
+
+---------- MODO COM FUNCTION COMPLICADO ------------
+
+let fmedia = function(n1:number,n2:number, n3:number):number{
     return (n1+n2+n3)/3
 }
 console.log(`Sua media e : ${fmedia (n1,n2,n3)}`)*/
